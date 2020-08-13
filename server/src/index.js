58240a44ddfac3edcf5d88b8ab2723e639f7df59
Anton Smirnov <antosha-85@yandex.ru>
2020-08-13 12:52:21 -0600

    Installed typescript and ts-node, types/node and types/express and created a tsconfig.json and configured with all the necessary setting as of right now

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 const port = 9000;
 
+const one = 1;
+let two = 2;
+two = 'two'
 app.get('/', (req, res) => {
-    res.send('Hello world, people!')
+    res.send(`1 + 2 = ${one + two}`)
 })
 
-app.listen(port, (req, res) => {
-    // console.log('listening on port ' + port)
+app.listen(port, function () {
+    console.log('listening on port ')
 })
 
 console.log(`[app]: htpp://localhost:${port}`)
+
