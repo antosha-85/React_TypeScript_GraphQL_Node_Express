76144113178f0bc518c2656f0482bca88e98dbbd
Anton Smirnov <antosha-85@yandex.ru>
2020-08-14 14:32:08 -0600

    passed newly created schema to our server and when we navigate to /api we are directed to a new graphical interface and we can make new queries

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 // import { listings } from "./listings";
 import { ApolloServer } from "apollo-server-express";
+import {schema} from './qraphql'
+
 // import bodyParser from "body-parser";
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer();
+const server = new ApolloServer({schema});
 server.applyMiddleware({ app, path: "/api" });
 
 // app.use(bodyParser.json());
