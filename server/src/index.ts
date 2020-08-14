5fe93c9c10ea0c3ac8ef52db464f5bd78251495b
Anton Smirnov <antosha-85@yandex.ru>
2020-08-14 14:26:03 -0600

    Imported ApolloServer and created a new variable, created a new file graphql.ts and created a new schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,30 +1,34 @@
 import express from "express";
-import { listings } from "./listings";
-import bodyParser from "body-parser";
+// import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+// import bodyParser from "body-parser";
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
+const server = new ApolloServer();
+server.applyMiddleware({ app, path: "/api" });
+
+// app.use(bodyParser.json());
 
 const one: number = 1;
 const two: number = 2;
 
 app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
 
-//listing
-app.get("/listings", (_req, res) => {
-  return res.send(listings);
-});
-//delete-listing
-app.post("/delete-listing", (req, res) => {
-  const id:string = req.body.id;
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send('Failed to delete!')
-});
+// //listing
+// app.get("/listings", (_req, res) => {
+//   return res.send(listings);
+// });
+// //delete-listing
+// app.post("/delete-listing", (req, res) => {
+//   const id: string = req.body.id;
+//   for (let i = 0; i < listings.length; i++) {
+//     if (listings[i].id === id) {
+//       return res.send(listings.splice(i, 1));
+//     }
+//   }
+//   return res.send("Failed to delete!");
+// });
 console.log(`[app]: htpp://localhost:${port}`);
 
 app.listen(port, () => {
