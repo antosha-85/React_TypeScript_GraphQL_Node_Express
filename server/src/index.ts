ac2aeeb531ef7e3a58731ec9e9cd5bd6c2462fea
Anton Smirnov <antosha-85@yandex.ru>
2020-08-13 22:20:50 -0600

    Created a new file listing.ts and created a new interface and pointed data types inside the object and applied that interface to our variable listings

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,23 @@
-// const express = require("express");
-import express from 'express'
+// const express = require("express")
+// type Listing = {}
+
+
+
+import express from "express";
 const app = express();
 const port = 9000;
 
-const one:number = 1;
-const two:number = 2;
+const one: number = 1;
+const two: number = 2;
 
-app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`))
+app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
 
 // app.listen(port, function () {
 //     console.log('listening on port ')
 // })
 
-console.log(`[app]: htpp://localhost:${port}`)
+console.log(`[app]: htpp://localhost:${port}`);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
-  });
+  console.log(`Hey people! My app is listening on port ${port}!`);
+});
