55f5be41975040627afb18521b0a2b416a732e58
Anton Smirnov <antosha-85@yandex.ru>
2020-08-13 21:41:11 -0600

    Updated setting for eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,7 @@ const port = 9000;
 const one:number = 1;
 const two:number = 2;
 
-app.get('/', (req, res) => {
-    res.send(`1 + 2 = ${one + two}`)
-})
+app.get('/', (_req, res) => res.send(`1 + 2 = ${one + two}`))
 
 // app.listen(port, function () {
 //     console.log('listening on port ')
