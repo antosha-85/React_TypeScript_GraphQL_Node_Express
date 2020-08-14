5a6575a5a6bbdb7b91fb273fbbf829e4b6b71d6d
Anton Smirnov <antosha-85@yandex.ru>
2020-08-13 22:56:43 -0600

    Created two request get that shows our data and post that deletes our data, for testing I used curl

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,32 @@
-// const express = require("express")
-// type Listing = {}
-
-
-
 import express from "express";
+import { listings } from "./listings";
+import bodyParser from "body-parser";
 const app = express();
 const port = 9000;
+
+app.use(bodyParser.json());
 
 const one: number = 1;
 const two: number = 2;
 
 app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
 
-// app.listen(port, function () {
-//     console.log('listening on port ')
-// })
-
+//listing
+app.get("/listings", (_req, res) => {
+  return res.send(listings);
+});
+//delete-listing
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+  }
+  return res.send('Failed to delete!')
+});
 console.log(`[app]: htpp://localhost:${port}`);
 
 app.listen(port, () => {
-  console.log(`Hey people! My app is listening on port ${port}!`);
+  // console.log(`Hey people! My app is listening on port ${port}!`);
 });
