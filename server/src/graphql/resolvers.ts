0a4657d6f1404aaebaadf72a9df93b98f27cda98
Anton Smirnov <antosha-85@yandex.ru>
2020-08-17 14:51:54 -0600

    Updated resolvers.ts file with Query and Mutation by providing the third context argument, which is our database

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,20 +1,31 @@
-import {IResolvers} from 'apollo-server-express'
-import {listings} from '../listings'
-
+import { IResolvers } from "apollo-server-express";
+import { Database } from "../lib/types";
+import { ObjectId } from "mongodb";
 export const resolvers: IResolvers = {
-    Query: {
-        listings: () => {
-            return listings
-        }
+  Query: {
+    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.listings.find({}).toArray();
     },
-    Mutation: {
-        deleteListing: (_root:undefined, { id }: {id: string})=> {
-            for (let i = 0; i < listings.length; i++) {
-                if(listings[i].id === id) {
-                    return listings.splice(i, 1)[0]
-                }
-            }
-            throw new Error('Failed to delete listing')
-        }
+  },
+  Mutation: {
+    deleteListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ) => {
+      //   for (let i = 0; i < listings.length; i++) {
+      //     if (listings[i].id === id) {
+      //       return listings.splice(i, 1)[0];
+      //     }
+      //   }
+      //   throw new Error("Failed to delete listing");
+      const deleteResult = await db.listings.findOneAndDelete({
+        _id: new ObjectId(),
+      });
+    if(!deleteResult) {
+        throw new Error('failed to delete listing')
     }
-}
+      return deleteResult.value
+    },
+  },
+};
