34779d4c3f4ed3feb0a94dac3d09241c214b4ce3
Anton Smirnov <antosha-85@yandex.ru>
2020-08-17 15:14:13 -0600

    Updated Listin in our resolvers

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,9 +1,13 @@
 import { IResolvers } from "apollo-server-express";
-import { Database } from "../lib/types";
+import { Database, Listing } from "../lib/types";
 import { ObjectId } from "mongodb";
 export const resolvers: IResolvers = {
   Query: {
-    listings: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+    listings: async (
+      _root: undefined,
+      _args: {},
+      { db }: { db: Database }
+    ): Promise<Listing[]> => {
       return await db.listings.find({}).toArray();
     },
   },
@@ -12,20 +16,19 @@ export const resolvers: IResolvers = {
       _root: undefined,
       { id }: { id: string },
       { db }: { db: Database }
-    ) => {
-      //   for (let i = 0; i < listings.length; i++) {
-      //     if (listings[i].id === id) {
-      //       return listings.splice(i, 1)[0];
-      //     }
-      //   }
-      //   throw new Error("Failed to delete listing");
+    ): Promise<Listing> => {
       const deleteResult = await db.listings.findOneAndDelete({
-        _id: new ObjectId(),
+        _id: new ObjectId(id),
       });
-    if(!deleteResult) {
-        throw new Error('failed to delete listing')
-    }
-      return deleteResult.value
+      if (!deleteResult.value) {
+        throw new Error("failed to delete listing");
+      }
+      return deleteResult.value;
     },
+  },
+  Listing: {
+    title: (listing: Listing) => listing.title,
+    image: (listing: Listing) => listing.image,
+    id: (listing: Listing): string => listing._id.toString(),
   },
 };
