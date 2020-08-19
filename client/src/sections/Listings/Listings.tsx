018fab2808307c0669c6a947897e58b94b9ffa42
Anton Smirnov <antosha-85@yandex.ru>
2020-08-18 22:50:59 -0600

    Imported FunctionComponent and ReactNode and used them to create a new Listings2 component but decided to comment it out and keep using Listing component

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { FunctionComponent, ReactNode } from 'react'
 
 interface Props {
     title:string;
+    // children: ReactNode
 }
 export const Listings = ({title}: Props) => {
     return <h2>{title}</h2>
 }
+// export const Listings2:FunctionComponent<Props> = ({title, children}) => {
+//     return <h2>{title}</h2>
+// }
