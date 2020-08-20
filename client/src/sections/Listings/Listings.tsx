a7afd87da43dedb4c41dff784c498bdd1e5edfd3
Anton Smirnov <antosha-85@yandex.ru>
2020-08-19 22:27:49 -0600

    Created a lib/api/server.ts file and created a fetch method, that fetches data and returns it and then when clicked on button it calls that method and logs the data in console, I got an error that something is wrong with my syntax- unexpected end of JSON input

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,12 +1,39 @@
-import React, { FunctionComponent, ReactNode } from 'react'
+import React from "react";
+import { server } from "../../lib/api/server";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 interface Props {
-    title:string;
-    // children: ReactNode
+  title: string;
 }
-export const Listings = ({title}: Props) => {
-    return <h2>{title}</h2>
-}
+
+export const Listings = ({ title }: Props) => {
+  const fetchListings = async() => {
+    const { data } = await server.fetch({ query: LISTINGS });
+    console.log(data); // check the console to see the listings data from our GraphQL Request!
+  };
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query Listings!</button>
+    </div>
+  );
+};
 // export const Listings2:FunctionComponent<Props> = ({title, children}) => {
 //     return <h2>{title}</h2>
 // }
