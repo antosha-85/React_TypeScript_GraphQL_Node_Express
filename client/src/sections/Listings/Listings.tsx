0e19c061c6fe2e9696af71a4ba1f031427712b89
Anton Smirnov <antosha-85@yandex.ru>
2020-08-21 22:03:09 -0600

    Created useEffect hook and called fetchListings inside

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { server } from "../../lib/api/server";
 import {
   ListingsData,
@@ -36,28 +36,33 @@ interface Props {
 export const Listings = ({ title }: Props) => {
   const [listings, setListings] = useState<Listing[] | null>(null);
 
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
   const fetchListings = async () => {
     const { data } = await server.fetch<ListingsData>({ query: LISTINGS });
     console.log(data.listings);
     setListings(data.listings); // check the console to see the listings data from our GraphQL Request!
   };
-  const deleteListing = async (id:string) => {
-    await server.fetch<
-      DeleteListingData,
-      DeleteListingVariables
-    >({
+  const deleteListing = async (id: string) => {
+    await server.fetch<DeleteListingData, DeleteListingVariables>({
       query: DELETE_LISTING,
       variables: {
         id,
       },
     });
-    fetchListings()
+    fetchListings();
   };
   const listingsList = listings ? (
     <ul>
       {listings.map((listing) => {
-        return <li key={listing.id}>{listing.title}
-        <button onClick={() => deleteListing(listing.id)}>Delete</button></li>;
+        return (
+          <li key={listing.id}>
+            {listing.title}
+            <button onClick={() => deleteListing(listing.id)}>Delete</button>
+          </li>
+        );
       })}
     </ul>
   ) : null;
@@ -66,7 +71,7 @@ export const Listings = ({ title }: Props) => {
     <div>
       <h2>{title}</h2>
       {listingsList}
-      <button onClick={fetchListings}>Query Listings!</button>
+      {/* <button onClick={fetchListings}>Query Listings!</button> */}
       {/* <button onClick={deleteListing}>Delete Listing</button> */}
     </div>
   );
