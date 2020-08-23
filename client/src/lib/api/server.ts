df60e0cb1b6910c30f4a8b85739fc18c7f0b8e60
Anton Smirnov <antosha-85@yandex.ru>
2020-08-22 23:44:42 -0600

    Fixed the problem with error for both cases when the server is not working or with a failed request status 200

--- client/src/lib/api/server.ts
+++ client/src/lib/api/server.ts
@@ -1,19 +1,23 @@
 interface Body<TVariables> {
-    query: string;
-    variables?: TVariables
-  }
-
-  export const server = {
-    fetch: async<TData = any, TVariables = any> (body: Body<TVariables>) => {
-      const res = await fetch("api", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(body)
-      });
-
-      return res.json() as Promise<{data: TData}>;
-      // return <Promise<{data: TData}>>res.json(); alternative way of type assertion
+  query: string;
+  variables?: TVariables;
+}
+interface Error {
+  message: string;
+}
+export const server = {
+  fetch: async <TData = any, TVariables = any>(body: Body<TVariables>) => {
+    const res = await fetch("api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch from server!");
     }
-  };
+    return res.json() as Promise<{ data: TData; errors: Error[] }>;
+    // return <Promise<{data: TData}>>res.json(); alternative way of type assertion
+  },
+};
