d28747b9ed6207d5cd42093c508b6ad9e3c6835e
Anton Smirnov <antosha-85@yandex.ru>
2020-08-26 00:02:28 -0600

    Modifmied skeleton margins

--- client/src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.tsx
+++ client/src/sections/Listings/components/ListingsSkeleton/ListingsSkeleton.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Divider, Skeleton } from "antd";
-
+import './styles/ListingsSkeleton.css'
 interface Props {
   title: string;
 }
 export const ListingsSkeleton = ({ title }: Props) => {
   return (
-    <div>
+    <div className='listings-skeleton'>
       <h2>{title}</h2>
       <Skeleton active paragraph={{rows: 1}}/>
       <Divider/>
