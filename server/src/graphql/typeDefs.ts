3ded70705c5e8fe68a9181ebc4fb97012045e785
Anton Smirnov <antosha-85@yandex.ru>
2020-08-31 13:43:20 -0600

    Created new query and new mutations that return a string and then exported those resolvers and checked that they are working properly

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -2,7 +2,10 @@ import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
   type Query {
+    authUrl: String!
   }
   type Mutation {
+    logIn: String!
+    logOut: String!
   }
 `;
