021add2957e15df7a35ec479107c66c33bdf9bce
Anton Smirnov <antosha-85@yandex.ru>
2020-08-31 15:11:25 -0600

    Updated resolvers for Viewer that converts id into _id and hasWallet into boolean value

--- server/src/graphql/resolvers/Viewer/index.ts
+++ server/src/graphql/resolvers/Viewer/index.ts
@@ -1,10 +1,10 @@
 import { IResolvers } from "apollo-server-express";
-
+import { Viewer } from "../../../lib/types";
 export const viewerResolvers: IResolvers = {
   Query: {
     authUrl: () => {
       return "Query.authUrl";
-    }
+    },
   },
   Mutation: {
     logIn: () => {
@@ -12,6 +12,14 @@ export const viewerResolvers: IResolvers = {
     },
     logOut: () => {
       return "Mutation.logOut";
-    }
-  }
+    },
+  },
+  Viewer: {
+    id: (viewer: Viewer): string | undefined => {
+      return viewer._id;
+    },
+    hasWallet: (viewer: Viewer): boolean | undefined => {
+      return viewer.walletId ? true : undefined;
+    },
+  },
 };
