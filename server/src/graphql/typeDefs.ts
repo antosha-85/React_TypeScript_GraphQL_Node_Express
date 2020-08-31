3f5d07a93a4373cea28774e551f4244b6764efdc
Anton Smirnov <antosha-85@yandex.ru>
2020-08-31 14:45:58 -0600

    Updated typeDefinitions for our server, made login and logout return a type Viewer

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,11 +1,24 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+  # person who's viewing our site, only didRequest is optional because we can see information without being logged in
+  type Viewer {
+    id: ID
+    token: String
+    avatar: String
+    hasWallet: boolean
+    didRequest: boolean!
+  }
+  # create a new input object to contain a required code
+  input LogInInput {
+    code: String!
+  }
+
   type Query {
     authUrl: String!
   }
   type Mutation {
-    logIn: String!
-    logOut: String!
+    logIn(input: LogInInput): Viewer!
+    logOut: Viewer!
   }
 `;
