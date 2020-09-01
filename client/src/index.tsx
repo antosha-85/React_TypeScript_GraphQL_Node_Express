cb117d4d24d9133e52eb6d3235724054df3e3f60
Anton Smirnov <antosha-85@yandex.ru>
2020-09-01 16:00:12 -0600

    Finished working on the login component and applied Layout for our add to center it

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,16 @@ import { render } from "react-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Home, Host, Listing, Listings, NotFound, User, Login } from "./sections";
+import { Layout } from "antd";
+import {
+  Home,
+  Host,
+  Listing,
+  Listings,
+  NotFound,
+  User,
+  Login,
+} from "./sections";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
@@ -14,15 +23,17 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/host" component={Host} />
-        <Route exact path="/listing/:id" component={Listing} />
-        <Route exact path="/listings/:location?" component={Listings} />
-        <Route exact path="/user/:id" component={User} />
-        <Route exact path="/login" component={Login} />
-        <Route component={NotFound} />
-      </Switch>
+      <Layout id="app">
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/host" component={Host} />
+          <Route exact path="/listing/:id" component={Listing} />
+          <Route exact path="/listings/:location?" component={Listings} />
+          <Route exact path="/user/:id" component={User} />
+          <Route exact path="/login" component={Login} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
     </Router>
   );
 };
@@ -30,7 +41,7 @@ render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       {/* <Listings title="Listings" /> */}
-      <App/>
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
